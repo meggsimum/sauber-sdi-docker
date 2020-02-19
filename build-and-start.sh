7834636302288184731fed161ab540a41bde936f
Christian Mayer <chris@meggsimum.de>
2020-02-19 13:24:35 +0100

    Add um_ol_demo image to build script

--- build-and-start.sh
+++ build-and-start.sh
@@ -11,5 +11,6 @@ docker build --rm -f "lubw_import/Dockerfile" -t lubw_download:latest "lubw_impo
 #docker build --rm -f "postgrest/Dockerfile" -t sauber_postgrest:latest "postgrest"
 #docker build --rm -f "postgrest/Dockerfile" -t postgrest_here:latest "postgrest"
 
+docker build --rm -f "um_ol_demo/Webmap.dockerfile" -t sauber_um_ol_demo:latest "um_ol_demo"
 
 docker stack deploy -c docker-stack.yml sauber-stack
