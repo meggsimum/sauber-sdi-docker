c29cf44b9560470156ed882699921c5cad3386f7
Julian Käflein <jkaeflein@geomer.de>
2020-03-24 16:35:24 +0100

    Create HeartbeatChannel on UM Server on deploy
    Trying to create an existing channel throws error, which would be irrelevant here. If this workflow persists, either check for existing channels or send error message to dev/null as well.

--- build-and-start.sh
+++ build-and-start.sh
@@ -14,3 +14,9 @@ docker build --rm -f "lubw_import/Dockerfile" -t lubw_download:latest "lubw_impo
 docker build --rm -f "um_ol_demo/Webmap.dockerfile" -t sauber_um_ol_demo:latest "um_ol_demo"
 
 docker stack deploy -c docker-stack.yml sauber-stack
+
+UM_SERVER_ID=`docker ps | grep um_server | cut -c1-5` # Get container ID of UM-Server
+
+if docker inspect -f '{{.State.Running}}' $UM_SERVER_ID > /dev/null ; then # Wait for UM Server to be up and running (State.Running = true)
+    docker exec $UM_SERVER_ID runUMTool.sh CreateChannel -rname=nsp://localhost:9000 -channelname=HeartbeatChannel # Create Heartbeat Channel on UM Server
+fi
