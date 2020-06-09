b3f7b802fb82df304cc346ffb009af6b97c81f1d
Julian Käflein <jkaeflein@geomer.de>
2020-06-09 11:34:24 +0200

    Reactivate PostgREST build

--- build-and-start.sh
+++ build-and-start.sh
@@ -6,7 +6,8 @@ docker build --rm -f "db/Dockerfile" -t sauber_postgis_alpine:latest "db"
 
 docker build --rm -f "raster_download/Dockerfile" -t raster_download:latest "raster_download"
 
-#docker build --rm -f "postgrest/Dockerfile" -t sauber_postgrest:latest "postgrest"
+docker build --rm -f "postgrest/Dockerfile" -t sauber_postgrest:latest "postgrest"
+
 #docker build --rm -f "postgrest/Dockerfile" -t postgrest_here:latest "postgrest"
 
 docker build --rm -f "um_ol_demo/Webmap.dockerfile" -t sauber_um_ol_demo:latest "um_ol_demo"
