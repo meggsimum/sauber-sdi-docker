7d25e438ff283acd4234e0a8c5c99bf9beeefdfb
Christian Mayer <chris@meggsimum.de>
2020-10-07 16:53:35 +0200

    Use TAG env var for Docker image tags in stack
    This ensures that the environment variable TAG can be used to modify the
    Docker image tag used for the images in the Docker stack. Default is
    "master".
    
    Also ensures that in the build scripts for the Docker images a tag
    "master" is set to the images.

--- build-images.sh
+++ build-images.sh
@@ -6,39 +6,62 @@ PUSH_TO_HUB=0
 DATE=`date +"%Y%m%d"`
 SALT="1"
 # version a la 20200701_1
-IMG_VERSION=$DATE"_"$SALT
+DATE_TAG=$DATE"_"$SALT
+# the tag for the latest git stage in master branch
+MASTER_TAG="master"
 
-echo "USING THE FOLLOWING VERSION FOR IMAGE BUILD "$IMG_VERSION
+echo "USING THE FOLLOWING TAG FOR IMAGE BUILD "$MASTER_TAG
+echo "USING THE FOLLOWING TAG FOR IMAGE BUILD "$DATE_TAG
 
-docker build --rm -f "db/Dockerfile" -t sauberprojekt/postgis_alpine:$IMG_VERSION "db"
+docker build --rm -f "db/Dockerfile" -t sauberprojekt/postgis_alpine:$MASTER_TAG "db"
+docker tag sauberprojekt/postgis_alpine:$MASTER_TAG sauberprojekt/postgis_alpine:$DATE_TAG
 
-docker build --rm -f "raster_download/Dockerfile" -t sauberprojekt/raster_download:$IMG_VERSION "raster_download"
+docker build --rm -f "raster_download/Dockerfile" -t sauberprojekt/raster_download:$MASTER_TAG "raster_download"
+docker tag sauberprojekt/raster_download:$MASTER_TAG sauberprojekt/raster_download:$DATE_TAG
 
-docker build --rm -f "json_download/Dockerfile" -t sauberprojekt/json_download:$IMG_VERSION "json_download"
+docker build --rm -f "json_download/Dockerfile" -t sauberprojekt/json_download:$MASTER_TAG "json_download"
+docker tag sauberprojekt/json_download:$MASTER_TAG sauberprojekt/json_download:$DATE_TAG
 
-docker build --rm -f "test_messenger/Dockerfile" -t sauberprojekt/test_messenger:$IMG_VERSION "test_messenger"
+docker build --rm -f "test_messenger/Dockerfile" -t sauberprojekt/test_messenger:$MASTER_TAG "test_messenger"
+docker tag sauberprojekt/test_messenger:$MASTER_TAG sauberprojekt/test_messenger:$DATE_TAG
 
-docker build --rm -f "postgrest/Dockerfile" -t sauberprojekt/postgrest:$IMG_VERSION "postgrest"
+docker build --rm -f "postgrest/Dockerfile" -t sauberprojekt/postgrest:$MASTER_TAG "postgrest"
+docker tag sauberprojekt/postgrest:$MASTER_TAG sauberprojekt/postgrest:$DATE_TAG
 
-docker build --rm -f "geoserver_publisher/Dockerfile" -t sauberprojekt/geoserver_raster_publisher:$IMG_VERSION "geoserver_publisher"
+docker build --rm -f "geoserver_publisher/Dockerfile" -t sauberprojekt/geoserver_raster_publisher:$MASTER_TAG "geoserver_publisher"
+docker tag sauberprojekt/geoserver_raster_publisher:$MASTER_TAG sauberprojekt/geoserver_raster_publisher:$DATE_TAG
 
-docker build --rm -f "um_ol_demo/Webmap.dockerfile" -t sauberprojekt/um_ol_demo:$IMG_VERSION "um_ol_demo"
+docker build --rm -f "um_ol_demo/Webmap.dockerfile" -t sauberprojekt/um_ol_demo:$MASTER_TAG "um_ol_demo"
+docker tag sauberprojekt/um_ol_demo:$MASTER_TAG sauberprojekt/um_ol_demo:$DATE_TAG
 
-docker build --rm -f "um-js-demo-client/Dockerfile" -t sauberprojekt/um_js_demo:$IMG_VERSION "um-js-demo-client"
+docker build --rm -f "um-js-demo-client/Dockerfile" -t sauberprojekt/um_js_demo:$MASTER_TAG "um-js-demo-client"
+docker tag sauberprojekt/um_js_demo:$MASTER_TAG sauberprojekt/um_js_demo:$DATE_TAG
 
 if [ $PUSH_TO_HUB -eq 1 ]
 then
   echo "Push images to hub.docker"
 
-  docker push sauberprojekt/postgis_alpine:$IMG_VERSION
+  docker push sauberprojekt/postgis_alpine:$MASTER_TAG
+  docker push sauberprojekt/postgis_alpine:$DATE_TAG
 
-  docker push sauberprojekt/raster_download:$IMG_VERSION
+  docker push sauberprojekt/raster_download:$MASTER_TAG
+  docker push sauberprojekt/raster_download:$DATE_TAG
 
-  docker push sauberprojekt/postgrest:$IMG_VERSION
+  docker push sauberprojekt/json_download:$MASTER_TAG
+  docker push sauberprojekt/json_download:$DATE_TAG
 
-  docker push sauberprojekt/geoserver_raster_publisher:$IMG_VERSION
+  docker push sauberprojekt/test_messenger:$MASTER_TAG
+  docker push sauberprojekt/test_messenger:$DATE_TAG
 
-  docker push sauberprojekt/um_ol_demo:$IMG_VERSION
+  docker push sauberprojekt/postgrest:$MASTER_TAG
+  docker push sauberprojekt/postgrest:$DATE_TAG
 
-  docker push sauberprojekt/um_js_demo:$IMG_VERSION
+  docker push sauberprojekt/geoserver_raster_publisher:$MASTER_TAG
+  docker push sauberprojekt/geoserver_raster_publisher:$DATE_TAG
+
+  docker push sauberprojekt/um_ol_demo:$MASTER_TAG
+  docker push sauberprojekt/um_ol_demo:$DATE_TAG
+
+  docker push sauberprojekt/um_js_demo:$MASTER_TAG
+  docker push sauberprojekt/um_js_demo:$DATE_TAG
 fi
