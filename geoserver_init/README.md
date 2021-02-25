21103ce9f0dfa4fd4d2060de4fc5be0554e98de4
Christian Mayer <chris@meggsimum.de>
2021-02-25 16:33:05 +0100

    Add geoserver_init script
    This adds a basic script to initialize the SAUBER GeoServer.

---
+++ geoserver_init/README.md
@@ -0,0 +1,5 @@
+# geoserver_publisher
+
+http://159.69.72.183:8080/geoserver/rest/workspaces/imagemosaic_test/coveragestores/nrw_no2/coverages/nrw_no2_mosaic/index/granules.json
+
+http://159.69.72.183:3001/tab_log
---
+++ geoserver_init/js-utils/docker-secrets.js
@@ -0,0 +1,32 @@
+/*
+ * Util to handle Docker secrets.
+ * Heavily inspired by https://medium.com/better-programming/how-to-handle-docker-secrets-in-node-js-3aa04d5bf46e
+ * All KUDOS to the author of the article.
+ *
+ * @author C. Mayer, meggsimum
+ */
+import fs from 'fs';
+
+const dockerSecret = {};
+
+/**
+ * Reads out the Docker secret.
+ *
+ * @param {String} secretName
+ */
+dockerSecret.read = function read(secretName) {
+  try {
+    return fs.readFileSync(`/run/secrets/${secretName}`, 'utf8');
+  } catch(err) {
+    if (err.code !== 'ENOENT') {
+      console.error(`An error occurred while trying to read the secret: ${secretName}. Err: ${err}`);
+    } else {
+      console.warn(`Could not find the secret, probably not running in swarm mode: ${secretName}. Err: ${err}`);
+
+      return fs.readFileSync(`../secrets/${secretName}.txt`, 'utf8');
+    }
+    return false;
+  }
+};
+
+export default dockerSecret;
