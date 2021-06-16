ab87ad5ccd8e473b51a1623584a8df012c5ee4ca
Julian Kaeflein <jkaeflein@geomer.de>
2021-06-16 17:59:23 +0200

    Update fv_stations ownership

--- db/update-files.d/8_create_station_view.sql
+++ db/update-files.d/8_create_station_view.sql
@@ -21,5 +21,7 @@ JOIN station_data.lut_component c on p.fk_component = c.idpk_component
 JOIN station_data.lut_station s on p.fk_station = s.idpk_station 
 GROUP BY s.idpk_station, s.station_name, s.station_code, s.wkb_geometry;
 
+GRANT SELECT ON station_data.fv_stations TO app;
 GRANT SELECT ON station_data.fv_stations TO sauber_user;
-GRANT SELECT ON station_data.fv_stations TO anon;
+ALTER TABLE station_data.fv_stations OWNER TO sauber_manager;
+GRANT ALL ON TABLE station_data.fv_stations TO sauber_manager;
