0737298b4f16c47e14de5675331533b118d3edbe
Julian Kaeflein <jkaeflein@geomer.de>
2021-06-16 17:09:39 +0200

    Create 8_create_station_view.sql

---
+++ db/update-files.d/8_create_station_view.sql
@@ -0,0 +1,25 @@
+\c sauber_data
+
+CREATE OR REPLACE VIEW station_data.fv_stations (
+    idpk,
+    station_name,
+    station_code,
+    eu_id,
+    pollutants,
+    geom
+)
+AS 
+SELECT 
+    s.idpk_station,
+    s.station_name,
+    s.station_code,
+    s.eu_id, 
+    array_to_json(array_agg(DISTINCT c.component_name)) as pollutants,
+    s.wkb_geometry as geom
+FROM station_data.tab_prediction p
+JOIN station_data.lut_component c on p.fk_component = c.idpk_component 
+JOIN station_data.lut_station s on p.fk_station = s.idpk_station 
+GROUP BY s.idpk_station, s.station_name, s.station_code, s.wkb_geometry;
+
+GRANT SELECT ON station_data.fv_stations TO sauber_user;
+GRANT SELECT ON station_data.fv_stations TO anon;
