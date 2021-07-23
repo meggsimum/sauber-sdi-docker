0f49d1689633bb7a581497061c01386868463f51
Julian Kaeflein <jkaeflein@geomer.de>
2021-07-23 14:51:17 +0200

    Add downloader, parser for LANUV nitrogen monoxide vals
    Based on existing LANUV downloader
    Executed in same container
    Data is provided in +/- 24h turns - executed at 9am, 9pm
    
    + Add missing empty line to existing parser

--- lanuv_download/resources/scripts/dl_lanuv_no.sh
+++ lanuv_download/resources/scripts/dl_lanuv_no.sh
@@ -47,24 +47,31 @@ for e in "${envs[@]}"; do
 done
 
 # Set dir and outfile pattern
+RAW_DIR=/tmp
 OUTDIR=/lanuv_data
-OUTFILE=lanuv_$(TZ=Europe/Berlin date +%Y%m%d%H%M).csv
+OUTFILE=$(TZ=Europe/Berlin date +%Y%m%d%H%M).csv
 
 # Download xml file from server. Exit if curl throws failure (http response!=2xx)
-if curl -s --fail "https://www.lanuv.nrw.de/fileadmin/lanuv/luft/immissionen/aktluftqual/eu_luftqualitaet.csv" -o $OUTDIR/$OUTFILE;
+if curl -s --fail "https://www.lanuv.nrw.de/fileadmin/lanuv/luft/immissionen/aktluftqual/eu_luftqualitaet.csv" -o $RAW_DIR/$OUTFILE;
 then
-    echo "Downloaded $OUTDIR/$OUTFILE."
+    echo "Downloaded $RAW/$OUTFILE."
 else
     echo "Error: Could not download file."
         exit 1
 fi
 
-# Get timestamp of the file through parsing, assign to variable needed by database function
-DATA_TS=\'$(head -1 $OUTDIR/$OUTFILE | awk -F "[;]" '{print $(NF-1)"."$NF}' | awk -F "[.]" '{print $3"-"$2"-"$1,$4""}')\'
+# Convert from ISO-8859-1 to UTF8 
+iconv -f ISO-8859-1 -t UTF-8 $RAW_DIR/$OUTFILE > $OUTDIR/${OUTFILE}_utf8.csv
 
-#Delete first 2 lines to meet postgres req for csv, replace invalid characters
-sed -i -e "1d;2d;" -e "s/\*/-/g" -e "s/<//g" $OUTDIR/$OUTFILE
-sleep 1
+# Alter and replace in CSV for postgres compatibility 
+sed -i -e "1d;2d;" \ # Delete first two lines (comments = bad header)
+	-e "s/\*/-/g" \ # Asterisk for bad measurements > Null
+	-e "s/<//g" \ # Lower-than-char for measurements under threshold
+	-e "s/--//g" \  # Two dashes for emtpy meas.
+	-e 's/\[[^][]*\]//g' \ # Anything in brackets (contain special unit chars e.g. microgm)
+	-e '/^[[:space:]]*$/d' \ # Empty lines at end of CSV
+	$OUTDIR/$OUTFILE
+wait 
 
 # Upload data to postgres, call in-db parser
 PGPASSWORD=$APP_PASSWORD /usr/bin/psql -h db -U app -d sauber_data -c "\copy station_data.input_lanuv FROM $OUTDIR/$OUTFILE CSV DELIMITER ';' NULL '-' ENCODING 'latin-1';"
