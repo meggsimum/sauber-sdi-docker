3a1a242791d701c47e38ae780f2b840129815e03
Julian Kaeflein <42374466+jdk24@users.noreply.github.com>
2021-09-16 16:41:17 +0200

    Add wms preview as column to existing fv_stations

--- db/update-files.d/015_create_wms_call_view.sql
+++ db/update-files.d/015_create_wms_call_view.sql
@@ -1,10 +1,13 @@
 \c sauber_data
 
 /*
-Create view returning relative wms URLs including bbox and CQL filter on relevant station 
+Alter fv_stations to include column for wms calls.
+Need to drop first as adding cols via CREATE OR REPLACE means complaints about how we cant rename columns.
 */
 
-CREATE OR REPLACE VIEW station_data.fv_wms_calls
+DROP VIEW IF EXISTS station_data.fv_stations;
+
+CREATE OR REPLACE VIEW station_data.fv_stations
 AS WITH envelope AS (
          SELECT s.idpk_station AS idpk,
             s.station_name,
@@ -17,21 +20,30 @@ AS WITH envelope AS (
          SELECT envelope.idpk,
             envelope.station_code,
             envelope.station_name,
-            concat(round(st_xmin(envelope.env::box3d)), ',', round(st_ymin(envelope.env::box3d)), ',', round(st_xmax(envelope.env::box3d)), ',', round(st_ymax(envelope.env::box3d))) AS string
+            concat(
+            round(st_xmin(envelope.env::box3d)),',', 
+            round(st_ymin(envelope.env::box3d)),',',
+            round(st_xmax(envelope.env::box3d)),',',
+            round(st_ymax(envelope.env::box3d))
+            ) AS string
            FROM envelope
         )
- SELECT row_number() OVER () AS obj_id,
-    bbox.idpk AS station_id,
-    bbox.station_code,
-    bbox.station_name,
-    concat('geoserver/station_data/wms?service=WMS&version=1.1.0&request=GetMap&layers=basemap,fv_stations&bbox=', bbox.string, '&width=250&height=250&srs=EPSG%3A3035&styles=,fv_stations_wmscall&CQL_FILTER=INCLUDE;station_code=', quote_literal(bbox.station_code), '&FORMAT=image/png') AS wms_call
-   FROM bbox
-  ORDER BY bbox.idpk;
+SELECT s.idpk_station AS idpk,
+    s.station_name,
+    s.station_code,
+    s.eu_id,
+    array_to_json(array_agg(DISTINCT c.component_name)) AS pollutants,
+    concat('geoserver/station_data/wms?service=WMS&version=1.1.0&request=GetMap&layers=basemap,fv_stations&bbox=', bbox.string, '&width=250&height=250&srs=EPSG%3A3035&styles=,fv_stations_wmscall&CQL_FILTER=INCLUDE;station_code=', quote_literal(bbox.station_code), '&FORMAT=image/png') AS preview_wms_image,
+    s.wkb_geometry AS geom
+   FROM station_data.tab_prediction p
+     JOIN station_data.lut_component c ON p.fk_component = c.idpk_component
+     JOIN station_data.lut_station s ON p.fk_station = s.idpk_station
+     JOIN bbox on s.station_code = bbox.station_code
+  GROUP BY s.idpk_station, s.station_name, s.station_code, bbox.string, bbox.station_code, s.wkb_geometry;
 
 -- Permissions
 
-ALTER TABLE station_data.fv_wms_calls OWNER TO postgres;
-GRANT ALL ON TABLE station_data.fv_wms_calls TO postgres;
-GRANT SELECT ON TABLE station_data.fv_wms_calls TO app;
-GRANT SELECT ON TABLE station_data.fv_wms_calls TO sauber_user;
-GRANT SELECT ON TABLE station_data.fv_wms_calls TO sauber_manager;
+ALTER TABLE station_data.fv_stations OWNER TO sauber_manager;
+GRANT ALL ON TABLE station_data.fv_stations TO postgres;
+GRANT SELECT ON TABLE station_data.fv_stations TO app;
+GRANT SELECT ON TABLE station_data.fv_stations TO sauber_user;
